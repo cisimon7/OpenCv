b4c583d94a43773c3f5e67534543ae2967c03524
cisimon7 <simontaneous1@hotmail.com>
2021-03-09 20:40:16 +0100

    Box Transformation Matrix done

--- main.cpp
+++ main.cpp
@@ -1,25 +1,73 @@
 #include <iostream>
 
+#include <aruco.h>
+#include "block_centers.h"
 #include "side_functions.h"
 
 using namespace cv;
+using namespace aruco;
 
 int main() {
 
     std::string video_path = samples::findFile("data/video/baumer_video0043.avi");
-    std::string img1 = samples::findFile("data/images/img1.jpg");
-    std::string img2 = samples::findFile("data/images/img2.jpg");
-    Mat vid;
+    Mat frame, gray_frame;
+    std::vector<Marker> markers;
 
-    Mat image1 = imread(img1, IMREAD_COLOR);
-    Mat image2 = imread(img2, IMREAD_COLOR);
-    Mat image3 = imread(img1, IMREAD_COLOR);
+    VideoCapture cap(video_path);
+    bool ret = cap.read(frame);
 
-    std::vector imgVec = { image1, image2, image3};
+    resize_image(frame);
+    cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
 
-    Mat images = show_many_images(imgVec);
+    CameraParameters cameraParameters;
+    cameraParameters.readFromXMLFile("data/config/camera_parameters.yml");
 
-    display(images);
+    MarkerDetector detector;
+
+    // Finding frame with at least two markers, one for the lower box and another for the upper box
+    MarkerMapPoseTracker low_pos, high_pos;
+    while (ret) {
+        detector.setDictionary("ARUCO_MIP_36h12", 0.9);
+        detector.detect(frame, markers, cameraParameters);
+
+        auto poses = new_mm_tracker(cameraParameters);
+        low_pos  = poses[0];
+        high_pos = poses[1];
+
+        low_pos.estimatePose(markers);
+        high_pos.estimatePose(markers);
+
+        if (low_pos.getRTMatrix().size && high_pos.getRTMatrix().size) {
+           break;
+        }
+
+        ret = cap.read(frame);
+        detector.detect(frame, markers, cameraParameters);
+    }
+
+    Mat frame_copy = frame.clone();
+
+    detector.detect(frame, markers, cameraParameters);
+    draw_border_axis(frame, cameraParameters, markers);
+
+    std::cout << low_pos.getRTMatrix() << std::endl;
+
+//    display(frame_copy);
+
+    auto tvec1 = get_box_centre(low_pos, block1[2]);
+    auto tvec_img1 = from_3d_to_2d(tvec1, cameraParameters.CameraMatrix);
+
+    auto rvec_img1 = Mat(3, 1, CV_64FC1, euler1);
+    auto rvec_img2 = Mat(3, 1, CV_64FC1, euler2);
+
+    std::cout<< rvec_img1 << std::endl;
+    std::cout<< tvec_img1 << std::endl;
+
+    CvDrawingUtils::draw3dAxis(frame_copy, cameraParameters, rvec_img1, tvec_img1, 5);
+
+    vector<Mat> images = { frame, frame_copy };
+    auto collage = show_many_images(images);
+    display(collage);
 
     return 0;
 }
--- side_functions.h
+++ side_functions.h
@@ -4,9 +4,9 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
-#include <iostream>
-
 using namespace cv;
+using namespace std;
+using namespace aruco;
 
 void display(Mat &img)  {
     if(img.empty()) {
@@ -18,15 +18,14 @@ void display(Mat &img)  {
     waitKey(0); // Wait for a keystroke in the window
 }
 
-Mat resize_image(Mat &img, bool grayscale=false, float fx=0.5, float fy=0.5) {
+void resize_image(Mat &img, bool grayscale=false, float fx=0.5, float fy=0.5) {
     if (grayscale)
         cvtColor(img, img, COLOR_BGR2GRAY);
 
     resize(img, img, Size(),0.5,0.5);
-    return img;
 }
 
-Mat show_many_images(std::vector<cv::Mat> &vecMat) {
+Mat show_many_images(vector<Mat> &vecMat) {
 
     if (vecMat.empty())
         return Mat();
@@ -40,7 +39,7 @@ Mat show_many_images(std::vector<cv::Mat> &vecMat) {
 
     std::vector<int> heights{};
     heights.reserve(vecMat.size());
-    for (const Mat& img : vecMat )
+    for (const Mat& img : vecMat)
             heights.push_back(img.rows);
 
     int height = *std::max_element(heights.begin(), heights.end());
@@ -65,4 +64,57 @@ Mat show_many_images(std::vector<cv::Mat> &vecMat) {
     }
 
     return collage;
+}
+
+std::vector<MarkerMapPoseTracker> new_mm_tracker(const CameraParameters& camParams) {
+
+    auto low_pos   = MarkerMapPoseTracker();
+    auto high_pose = MarkerMapPoseTracker();
+
+    auto markerMap1 = MarkerMap("data/config/block1.yml");
+    auto markerMap2 = MarkerMap("data/config/block2.yml");
+
+    low_pos.setParams(camParams, markerMap1);
+    high_pose.setParams(camParams, markerMap2);
+
+    return { low_pos, high_pose };
+}
+
+void draw_border_axis(Mat& frame, const CameraParameters& cameraParameters, std::vector<Marker> markers) {
+
+    // draw border for each marker in the frame
+    for (auto & marker : markers) {
+        //std::cout << "\n" << marker << "\n" << std::endl;
+        marker.draw(frame, Scalar(0, 0, 255), 2);
+    }
+
+    // draw a 3d cube in each marker if there is 3d info
+    if (cameraParameters.isValid())
+        for (auto & marker : markers)
+            if (marker.isPoseValid()) {
+                CvDrawingUtils::draw3dAxis(frame, marker, cameraParameters);
+                CvDrawingUtils::draw3dCube(frame, marker, cameraParameters);
+            }
+}
+
+Mat get_box_centre(const MarkerMapPoseTracker& mmap, float center_pos[4]) {
+    auto rt_mat = mmap.getRTMatrix();
+
+    for (int i=0; i<3; i++)
+        center_pos[i] = center_pos[i]/1000;
+
+    std::cout << rt_mat.rowRange(0,3) << std::endl;
+    std::cout << Mat(4, 1, CV_32FC1, center_pos) << std::endl;
+
+    Mat result = rt_mat.rowRange(0,3) * Mat(4, 1, CV_32FC1, center_pos);
+
+    return result;
+}
+
+Mat from_3d_to_2d(const Mat& pos, const Mat& camera_transformer) {
+    Mat result = camera_transformer * pos.rowRange(0,3);
+
+    result = result/(result.at<float>(2,0));
+
+    return result/100;
 }
